2dec56245247f0c581ef9571703926532d9e20c5
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-09 22:35:46 -0300

    Correção APIMetrics
    Correção APIMetrics

--- Metrics/APIMetrics/Models/TargetValue.cs
+++ Metrics/APIMetrics/Models/TargetValue.cs
@@ -4,6 +4,6 @@ namespace APIMetrics.Models;
 
 public class TargetValue
 {
-    [Range(minimum: 1, maximum: 100, ErrorMessage = "Informe um valor entre 1 e 100")]
+    [Range(minimum: -100, maximum: 100, ErrorMessage = "Informe um valor entre -100 e 100")]
     public int NewTargetValue { get; set; }
 }
